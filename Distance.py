975621fb8f5ab8a4c9ddbb06bc01dfa142e1eb97
Tobias380 <tobias_38@hotmail.com>
2013-10-31 22:36:19 +0000

    Update Distance.py
    Inclusion of a tolerance measure and separated the distance measurements between the two ultrasonic sensors

--- Distance.py
+++ Distance.py
@@ -35,35 +35,104 @@ def Measure2 ():
     Distance2 =((stop- start)/0.000147)
     return Distance2
 
-def Average (t) :
+def Average1 () :
     distance1= Measure1()
-    time.sleep(t)
-    distance2= Measure2()
-    time.sleep(t)
+    time.sleep(0.01)
+    distance2= Measure1()
+    time.sleep(0.01)
     distance3= Measure1()
-    time.sleep(t)
-    distance4= Measure2()
-    time.sleep(t)
+    time.sleep(0.01)
+    distance4= Measure1()
+    time.sleep(0.01)
     distance5= Measure1()
-    time.sleep(t)
-    distance6= Measure2()
-    time.sleep(t)
+    time.sleep(0.01)
+    distance6= Measure1()
+    time.sleep(0.01)
     distance7= Measure1()
-    time.sleep(t)
-    distance8= Measure2()
-    time.sleep(t)
+    time.sleep(0.01)
+    distance8= Measure1()
+    time.sleep(0.01)
     distance9= Measure1()
-    time.sleep(t)
+    time.sleep(0.01)
+    distance10= Measure1()
+    AvgDist= ((distance1 + distance2 + distance3 + distance4 + distance5 + distance6 + distance7 + distance8 + distance9 + distance10)/10)
+    #AvgDist= ((distance1 + distance2 + distance3)/3)
+    Tolerance=AvgDist*0.1
+    if abs(distance1- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance2- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance3- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance4- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance5- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance6- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance7- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance8- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance9- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance10- AvgDist)> Tolerance
+       print ('Tolerance difference')
+      
+    return AvgDist
+
+def Average2 () :
+    distance1= Measure2()
+    time.sleep(0.01)
+    distance2= Measure2()
+    time.sleep(0.01)
+    distance3= Measure2()
+    time.sleep(0.01)
+    distance4= Measure2()
+    time.sleep(0.01)
+    distance5= Measure2()
+    time.sleep(0.01)
+    distance6= Measure2()
+    time.sleep(0.01)
+    distance7= Measure2()
+    time.sleep(0.01)
+    distance8= Measure2()
+    time.sleep(0.01)
+    distance9= Measure2()
+    time.sleep(0.01)
     distance10= Measure2()
     AvgDist= ((distance1 + distance2 + distance3 + distance4 + distance5 + distance6 + distance7 + distance8 + distance9 + distance10)/10)
     #AvgDist= ((distance1 + distance2 + distance3)/3)
+    Tolerance=AvgDist*0.1
+    if abs(distance1- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance2- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance3- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance4- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance5- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance6- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance7- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance8- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance9- AvgDist)> Tolerance
+       print ('Tolerance difference')
+    if abs(distance10- AvgDist)> Tolerance
+       print ('Tolerance difference')
 
     return AvgDist
+
 try:
-    while (True):
-        t= input ('Enter time gap: ')
-        Height = Average(t)
-        print ('Distance=', Height)
+    while (True)
+        Height1 = Average1()
+        Height2 = Average2()
+        print ('Distance=', Height1)
+        print ('Distance=', Height2)
 except KeyboardInterrupt :
     GPIO.cleanup()
 
